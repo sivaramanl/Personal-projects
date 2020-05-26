339cd02ecf92c54cd4bf3af7d6989542b78bee3d
Sivaraman Lakshmipathy <sivaramanl@users.noreply.github.com>
2020-05-26 14:53:06 -0500

    Batch file updated

--- Windows spotlight collector/Python version/setup.bat
+++ Windows spotlight collector/Python version/setup.bat
@@ -1,1 +1,9 @@
+@echo off
+schtasks /query /TN "Windows_spotlight_collector" >NUL 2>&1
+if %errorlevel% EQU 0 (
+schtasks /DELETE /TN "Windows_spotlight_collector" /f
 schtasks /CREATE /TN "Windows_spotlight_collector" /XML "%~dp0\windows_spotlight_copier.xml"
+) else (
+schtasks /CREATE /TN "Windows_spotlight_collector" /XML "%~dp0\windows_spotlight_copier.xml"
+)
+exit
